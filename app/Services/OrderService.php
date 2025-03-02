e50dc07b8e68a984b453c5f784c8b8655f0de4c0
yassin-gritli <gritli.yaseen@gmail.com>
2025-03-02 09:50:38 +0200

    fix: remove duplicate products

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -57,7 +57,6 @@ class OrderService
                         'customer_phone' => $group->first()['customer']['phone'] ?? $group->first()['customer']['mobile'],
                         'latitude' => $group->first()['location']['latitude'],
                         'longitude' => $group->first()['location']['longitude'],
-                        'products' => $group->first()['products'],
                         'products' => $group->map(function ($item) {
                             return $item['products'];
                         })->flatten(1)->toArray(),

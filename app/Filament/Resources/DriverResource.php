3d5af3bedf6b79e14cadf2bd9a4a1385417b81d3
yassin-gritli <gritli.yaseen@gmail.com>
2025-03-18 10:58:29 +0200

    feat: update DriverResource and OrderResource navigation icons and enhance Driver table columns

--- app/Filament/Resources/DriverResource.php
+++ app/Filament/Resources/DriverResource.php
@@ -2,22 +2,24 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\DriverResource\Pages;
-use App\Filament\Resources\DriverResource\RelationManagers;
-use App\Models\Driver;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Driver;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\DriverResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\DriverResource\RelationManagers;
 
 class DriverResource extends Resource
 {
     protected static ?string $model = Driver::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-truck';
 
     public static function form(Form $form): Form
     {
@@ -31,7 +33,38 @@ class DriverResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->label('Id'),
+
+                TextColumn::make('national_no')
+                    ->label('National No'),
+
+
+                TextColumn::make('full_name')
+                    ->label('Driver Name')
+                    ->getStateUsing(function ($record) {
+                        return $record->first_name . ' ' . $record->last_name;
+                    }),
+
+                TextColumn::make('phone')
+                    ->label('Phone'),
+
+                TextColumn::make('gender')
+                    ->label('Gender'),
+
+                IconColumn::make('is_active')
+                    ->boolean(),
+
+                TextColumn::make('driver_type')
+                    ->label('Driver Type'),
+
+                TextColumn::make('delivery_status')
+                    ->label('Delivery Status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'available' => 'success',
+                        'not_available' => 'warning',
+                    }),
             ])
             ->filters([
                 //

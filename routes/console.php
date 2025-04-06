fbcdacccb999c51286b0baa257a3160cba1cbc1a
yassin-gritli <gritli.yaseen@gmail.com>
2025-04-06 10:27:30 +0200

    chore: remove scheduled task for storing new JM orders

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,3 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
-Schedule::call(
-    fn(OrderService $orderService) => $orderService->storeNewJmOrders()
-)->everyMinute();

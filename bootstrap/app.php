18cf8328af2fa6d39d8b427f50e53d62fc5a1e5f
yassin-gritli <gritli.yaseen@gmail.com>
2025-04-07 11:41:56 +0200

    feat: trust specific proxy IP in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies([
+            '192.168.10.26',
+        ]);
         $middleware->api(prepend: [
             'App\Http\Middleware\ForceJsonResponse::class',
         ]);

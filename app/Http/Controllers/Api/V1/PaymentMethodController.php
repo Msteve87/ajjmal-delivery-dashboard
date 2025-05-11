630bc3ededbe0f04a8df467f671e98798f418a91
yassin-gritli <gritli.yaseen@gmail.com>
2025-05-11 14:59:27 +0200

    feat: add PaymentMethodController with index method and route for listing payment methods

---
+++ app/Http/Controllers/Api/V1/PaymentMethodController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use App\Models\PaymentMethod;
+use App\Http\Controllers\Controller;
+
+class PaymentMethodController extends Controller
+{
+    public function index()
+    {
+        $paymentMethods = PaymentMethod::query()
+            ->select('id', 'name', 'name_ar', 'code', 'icon')
+            ->get();
+
+        return response()->json(
+            [
+                'status' => 200,
+                'data' => $paymentMethods,
+            ],
+        );
+    }
+}
--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -3,11 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\OrderStatusController;
+use App\Http\Controllers\Api\V1\PaymentMethodController;
 
 Route::middleware('auth:sanctum')
     ->group(function () {
         Route::controller(OrderStatusController::class)
             ->group(function () {
                 Route::get('/order-statuses/list', [OrderStatusController::class, 'index']);
+            });
+
+        Route::controller(PaymentMethodController::class)
+            ->group(function () {
+                Route::get('/payment-methods/list', [PaymentMethodController::class, 'index']);
             });
     });

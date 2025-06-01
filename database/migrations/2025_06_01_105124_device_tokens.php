055ec1509fb1afbf2c39114b00ab82361ca059ed
yassin-gritli <gritli.yaseen@gmail.com>
2025-06-01 11:29:30 +0200

    feat: create device_tokens migration with necessary fields

--- database/migrations/2025_06_01_105124_device_tokens.php
+++ database/migrations/2025_06_01_105124_device_tokens.php
@@ -4,14 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::create('device_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('token')->unique();
+            $table->string('device_type')->nullable();
+            $table->foreignId('driver_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**

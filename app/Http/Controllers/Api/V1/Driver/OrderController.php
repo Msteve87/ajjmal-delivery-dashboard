c322ad5eacbc6b54f3bb1d7fadf43b708aa42f1f
yassin-gritli <gritli.yaseen@gmail.com>
2025-06-10 09:59:06 +0200

    refactor: remove Firebase messaging implementation from acceptOrder method

--- app/Http/Controllers/Api/V1/Driver/OrderController.php
+++ app/Http/Controllers/Api/V1/Driver/OrderController.php
@@ -11,10 +11,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification;
-
 class OrderController extends Controller
 {
     public function __construct(
@@ -147,20 +143,6 @@ class OrderController extends Controller
                     ]
                 );
             }
-
-            $deviceToken = $this->deviceTokenService->getDeviceToken(Auth::id());
-
-            $factory = (new Factory)->withServiceAccount(config('services.firebase.credentials'));
-
-            $messaging = $factory->createMessaging();
-
-            $message = CloudMessage::withTarget('token', $deviceToken)
-                ->withNotification(Notification::create(
-                    'Your Notification Title',
-                    'This is the message body.'
-                ));
-
-            $messaging->send($message);
 
             return response()->json(
                 [

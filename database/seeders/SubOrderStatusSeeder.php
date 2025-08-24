7b5f66939b7f181053ca3b59499459fdfedf2dbb
yassin-gritli <gritli.yaseen@gmail.com>
2025-08-24 15:04:07 +0200

    feat: add SubOrderStatusSeeder

---
+++ database/seeders/SubOrderStatusSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubOrderStatus;
+use App\Services\AjjmalMarketApiService;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubOrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $items = (new AjjmalMarketApiService())->getJmOrderStates();
+
+        $subOrderStatuesAr = collect($items)
+            ->where('id_lang', 2)
+            ->keyBy('id_order_state');
+
+        foreach ($items as $item) {
+            if ($item['id_lang'] != 1) {
+                continue;
+            }
+
+            SubOrderStatus::create([
+                "id" => $item["id_order_state"],
+                "name" => $item["name"],
+                "name_ar" => $subOrderStatuesAr[$item["id_order_state"]]['name'] ?? null,
+                "color" => $item["color"],
+            ]);
+        }
+    }
+}

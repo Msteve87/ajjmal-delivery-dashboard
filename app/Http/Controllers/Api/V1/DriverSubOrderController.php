1cdb375f8b822d2c8ba49cdc9badcc8fe985f29a
yassin-gritli <gritli.yaseen@gmail.com>
2025-09-18 09:51:44 +0200

    feat: add listDriverOrders to DriverSubOrder controller

---
+++ app/Http/Controllers/Api/V1/DriverSubOrderController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\SubOrder;
+use App\Http\Resources\Api;
+use App\Enums\JmOrderStatus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class DriverSubOrderController extends Controller
+{
+    public function listDriverOrders()
+    {
+        $subOrders = SubOrder::where('driver_id', Auth::id())
+            ->with('order')
+            ->whereIn('sub_order_status_id', [
+                JmOrderStatus::processingInProgress->value,
+                JmOrderStatus::awaitingCashOnDelivery->value
+            ])
+            ->get();
+
+        return response()->json(
+            [
+                'status' => 'success',
+                'data' => [
+                    'items' => Api\V1\SubOrderResource::collection($subOrders),
+                ],
+            ]
+        );
+    }
+}

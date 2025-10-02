44900053a39685b6a0efc81920069f621acf85b3
yassin-gritli <gritli.yaseen@gmail.com>
2025-10-02 12:13:33 +0200

    feat: implement form method in EditUser to define user input fields

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -4,11 +4,27 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Forms\Form;
+use Filament\Forms;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
 
     protected function getHeaderActions(): array
     {

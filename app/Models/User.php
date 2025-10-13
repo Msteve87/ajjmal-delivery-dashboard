a01de085162e264ae9d49047c8fd15ef04efc622
yassin-gritli <gritli.yaseen@gmail.com>
2025-10-13 15:51:40 +0200

    feat: add HasRoles trait to User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -24,6 +25,14 @@ class User extends Authenticatable implements FilamentUser
         'email',
         'password',
     ];
+
+    protected string $guard_name = 'sanctum';
+
+    protected function getDefaultGuardName(): string
+    {
+        return $this->guard_name;
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.

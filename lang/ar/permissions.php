90b50796fdb333092c3b5a7181fbb157bbe8385e
yassin-gritli <gritli.yaseen@gmail.com>
2025-10-13 15:50:54 +0200

    feat: add permissions localization

---
+++ lang/ar/permissions.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'add.settlement' => 'إضافة تسويات',
+    'browse.activity_log' => 'عرض سجلات النشاط',
+    'browse.own_activity_log' => 'عرض سجل النشاط الشخصي',
+    'add.user' => 'إضافة مستخدم',
+    'edit.user' => 'تعديل مستخدم',
+    'assign.delivery.tasks' => 'تعيين مهام التوصيل'
+];
---
+++ lang/en/permissions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'add.settlement' => 'Add Settelment',
+    'browse.activity_log' => 'Browse Activity Log',
+    'browse.own_activity_log' => 'Browse Own Activity Log',
+    'add.user' => 'Add User',
+    'edit.user' => 'Edit User',
+    'assign.delivery.tasks' => 'Assign delivery Tasks'
+];

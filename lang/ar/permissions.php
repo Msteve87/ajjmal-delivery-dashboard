eac101512b5421f3f82bfd03be1c219b152971da
yassin-gritli <gritli.yaseen@gmail.com>
2025-10-14 15:45:13 +0200

    feat: add more permissions localization

--- lang/ar/permissions.php
+++ lang/ar/permissions.php
@@ -6,5 +6,14 @@ return [
     'browse.own_activity_log' => 'عرض سجل النشاط الشخصي',
     'add.user' => 'إضافة مستخدم',
     'edit.user' => 'تعديل مستخدم',
-    'assign.delivery.tasks' => 'تعيين مهام التوصيل'
+    'assign.delivery.tasks' => 'تعيين مهام التوصيل',
+    'assign.permission' => 'تعيين صلاحية',
+    'browse.order' => 'عرض الطلبات',
+    'delete.user' => 'حذف مستخدم',
+    'browse.user' => 'عرض المستخدمين',
+    'add.driver' => 'إضافة سائق',
+    'edit.driver' => 'تعديل سائق',
+    'view.driver' => 'عرض سائق',
+    'browse.driver' => 'عرض السائقين',
+    'update.order.status' => 'تحديث حالة الطلب',
 ];
--- lang/en/permissions.php
+++ lang/en/permissions.php
@@ -7,5 +7,14 @@ return [
     'browse.own_activity_log' => 'Browse Own Activity Log',
     'add.user' => 'Add User',
     'edit.user' => 'Edit User',
-    'assign.delivery.tasks' => 'Assign delivery Tasks'
+    'assign.delivery.tasks' => 'Assign delivery Tasks',
+    'assign.permission' => 'Assign Permission',
+    'browse.order' => 'Browse Orders',
+    'delete.user' => 'Delete User',
+    'browse.user' => 'Browse Users',
+    'add.driver' => 'Add Driver',
+    'edit.driver' => 'Edit Driver',
+    'view.driver' => 'View Driver',
+    'browse.driver' => 'Browse Drivers',
+    'update.order.status' => 'Update Order Status',
 ];

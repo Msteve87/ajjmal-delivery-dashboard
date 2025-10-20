3995fe6d0dc7bede9e8004bc2e125dc8a9be866c
yassin-gritli <gritli.yaseen@gmail.com>
2025-10-20 10:03:22 +0200

    chore: disable edit and create pages in ActivityResource

--- app/Filament/Resources/ActivityResource.php
+++ app/Filament/Resources/ActivityResource.php
@@ -114,8 +114,8 @@ class ActivityResource extends Resource
     {
         return [
             'index' => Pages\ListActivities::route('/'),
-            'create' => Pages\CreateActivity::route('/create'),
-            'edit' => Pages\EditActivity::route('/{record}/edit'),
+            // 'create' => Pages\CreateActivity::route('/create'),
+            // 'edit' => Pages\EditActivity::route('/{record}/edit'),
         ];
     }
 

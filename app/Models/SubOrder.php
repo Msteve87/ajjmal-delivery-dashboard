f5e3b2d58101fc6c01b8d94d2ca86149a36d4970
yassin-gritli <gritli.yaseen@gmail.com>
2025-11-16 10:37:55 +0200

    fix: remove deliver at from deliverd scope

--- app/Models/SubOrder.php
+++ app/Models/SubOrder.php
@@ -36,8 +36,7 @@ class SubOrder extends Model
 
     public function scopeDelivered($query)
     {
-        return $query->where('sub_order_status_id', 5)
-            ->whereNotNull('delivered_at');
+        return $query->where('sub_order_status_id', 5);
     }
 
     public function scopeDeliveredToday($query)

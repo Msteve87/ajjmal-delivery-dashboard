6ecb5be557c7fa54f614fa2d617d021e3a5218f1
yassin-gritli <gritli.yaseen@gmail.com>
2025-11-16 10:26:21 +0200

    fix: correct SubOrder query scope

--- app/Models/SubOrder.php
+++ app/Models/SubOrder.php
@@ -36,7 +36,8 @@ class SubOrder extends Model
 
     public function scopeDelivered($query)
     {
-        return $query->whereNotNull('delivered_at');
+        return $query->where('sub_order_status_id', 5)
+            ->whereNotNull('delivered_at');
     }
 
     public function scopeDeliveredToday($query)

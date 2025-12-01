ab54370b743c633da34cbc3ff0c8c5c4e70ef658
yassin-gritli <gritli.yaseen@gmail.com>
2025-12-01 12:50:39 +0200

    refactor: remove settlement_id migration and update scheduling for JM orders

--- routes/console.php
+++ routes/console.php
@@ -7,26 +7,12 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::call(function (OrderService $orderService) {
-    $orders = $orderService->getJmOrders();
-
-    $refs = collect($orders)->pluck('reference')->filter()->values();
-
-    foreach ($refs as $ref) {
-        $orderService->storeNewJmOrderByRef($ref);
-    }
-})->everyThirtySeconds();
-
-// Schedule::call(function (OrderService $orderService) {
-//     $orderService->storeNewJmOrders();
-// })->everyThirtySeconds();
+    $orderService->storeNewJmOrders();
+})->everyMinute();
 
 
 Schedule::call(function (OrderService $orderService) {
     $orderService->updateJmOrders();
 })->everyMinute();
-

a71ccc8072a77d5953dabb1c5437a9a96574dba3
yassin-gritli <gritli.yaseen@gmail.com>
2025-12-02 13:15:22 +0200

    fix: correct discount calculation in cash and online delivery totals

--- app/Services/DriverService.php
+++ app/Services/DriverService.php
@@ -27,7 +27,8 @@ class DriverService
             })
             ->delivered()
             ->unsettled()
-            ->sum(DB::raw('total - COALESCE(discount,0)'));
+            ->sum(DB::raw('total - COALESCE(total_discounts,0)'));
+        ;
 
     }
 
@@ -45,7 +46,8 @@ class DriverService
             })
             ->delivered()
             ->unsettled()
-            ->sum(DB::raw('total - COALESCE(discount,0)'));
+            ->sum(DB::raw('total - COALESCE(total_discounts,0)'));
+        ;
 
     }
 
